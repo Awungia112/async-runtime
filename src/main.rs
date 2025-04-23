98668c4aaf0c7e6d30aef5dcb445ab40079caaf7
Awungia112 <awungiaf@gmail.com>
2025-04-23 08:02:48 +0100

    feat :async tasks demonstrating functionality

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use async_runtime::{mini_rt, sleep, spawn};
+use std::time::Duration;
+
+async fn task_one() {
+    println!("task one: start");
+    sleep(Duration::from_secs(1)).await;
+    println!("task one: done");
+}
+
+async fn task_two() {
+    println!("task two: start");
+    sleep(Duration::from_secs(2)).await;
+    println!("task two: done");
+}
+
+mini_rt! {
+    async fn main() {
+        let _ = spawn(async {
+            println!("Runtime started...");
+        }).await;
+        
+        task_one().await;
+        task_two().await;
+    }
+}
